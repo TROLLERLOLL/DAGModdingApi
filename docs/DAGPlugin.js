0ba4b84d590ed7a9873b4e0dd042bbd2f6d38afa
TROLLERLOLL <44126890+TROLLERLOLL@users.noreply.github.com>
2024-01-09 22:39:17 +0100

    Create DAGPlugin.js

---
+++ docs/DAGPlugin.js
@@ -0,0 +1,9 @@
+(function () {
+  var DAGPlugin = function (hook, vm) {
+    
+  };
+
+  // Add plugin to docsify's plugin array
+  $docsify = $docsify || {};
+  $docsify.plugins = [].concat($docsify.plugins || [], DAGPlugin);
+})();
