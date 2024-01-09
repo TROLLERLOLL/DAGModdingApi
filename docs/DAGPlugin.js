4f5e0f379d4cb5bcb551219591d29e4feb72a21c
TROLLERLOLL <44126890+TROLLERLOLL@users.noreply.github.com>
2024-01-09 22:47:43 +0100

    Added things

--- docs/DAGPlugin.js
+++ docs/DAGPlugin.js
@@ -1,9 +1,39 @@
-(function () {
-  var DAGPlugin = function (hook, vm) {
-    
-  };
+var DAGPlugin = function (hook, vm) {
+  hook.init(function() {
+    console.log("DAGP")
+  });
 
+  hook.afterEach(function(html) {
+    var selector = document.createElement('div');
+      selector.className = 'version-selector';
+      selector.innerHTML = `
+      <select>
+          ${versions
+          .map(
+              (v) =>
+              `<option value="${v.folder}" ${
+                  v.default ? 'selected' : ''
+              }>${v.label}</option>`
+          )
+          .join('')}
+      </select>
+      `;
+
+     return html + selector;
+  })
+};
+
+
+(function () {
   // Add plugin to docsify's plugin array
   $docsify = $docsify || {};
   $docsify.plugins = [].concat($docsify.plugins || [], DAGPlugin);
+})();
+
+(function() {
+  
+  if (window.location.pathname === window.$docsify.home || window.location.pathname === window.$docsify.home + '/index.html') {
+    var defaultVersion = window.$docsify.versions.find((v) => v.default).folder;
+    window.location.replace(window.$docsify.home + '#/' + defaultVersion + '/');
+  }
 })();
