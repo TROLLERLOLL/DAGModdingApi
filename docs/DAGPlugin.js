e57a96204a9a3e2c4fcd3824587a9bb4dee13090
TROLLERLOLL <44126890+TROLLERLOLL@users.noreply.github.com>
2024-01-10 18:49:34 +0100

    Update DAGPlugin.js

--- docs/DAGPlugin.js
+++ docs/DAGPlugin.js
@@ -1,38 +1,38 @@
-var DAGPlugin = function (hook, vm) {
-  hook.afterEach(function(html, next) {
-alert("5" + getVersionName());
-next(html.replace(/{{versionLabel}}/g, getVersionName()));
-});
-  hook.doneEach(function() {
+var DAGPlugin = function(hook, vm) {
+    hook.afterEach(function(html, next) {
+        alert("5" + getVersionName());
+        next(html.replace(/{{versionLabel}}/g, getVersionName()));
+    });
+    hook.doneEach(function() {
 
-var codeblocks = document.getElementsByTagName("pre");
+        var codeblocks = document.getElementsByTagName("pre");
 
-for (var i = 0; i < codeblocks.length; i++) {
- if (codeblocks[i].getAttribute("data-lang") == "csharp") 
-codeblocks[i].setAttribute("data-lang", "c#");
-}
-/*var nameEl1 = document.querySelector('.app-name');
-  if (nameEl1) {
-      var versionLabel2 = window.location.hash;// vm.config.versions.find((v) => window.location.hash.includes(v.folder)).label;
-      nameEl1.innerHTML += ` <small id="vlabel">${versionLabel2}</small>`;
-alert(versionLabel2);
-      //nameEl.parentNode.insertBefore(selector, nameEl.nextElementSibling);
-  }
-*/
-//return markdown;
-  });
+        for (var i = 0; i < codeblocks.length; i++) {
+            if (codeblocks[i].getAttribute("data-lang") == "csharp")
+                codeblocks[i].setAttribute("data-lang", "c#");
+        }
+        /*var nameEl1 = document.querySelector('.app-name');
+          if (nameEl1) {
+              var versionLabel2 = window.location.hash;// vm.config.versions.find((v) => window.location.hash.includes(v.folder)).label;
+              nameEl1.innerHTML += ` <small id="vlabel">${versionLabel2}</small>`;
+        alert(versionLabel2);
+              //nameEl.parentNode.insertBefore(selector, nameEl.nextElementSibling);
+          }
+        */
+        //return markdown;
+    });
 
-  hook.ready(function() {
-    var selector = initVersionSelector();
-  })
+    hook.ready(function() {
+        var selector = initVersionSelector();
+    })
 };
 
 function initVersionSelector() {
-  // Version selector
-  var versions = window.$docsify.versions;
-  var selector = document.createElement('div');
-  selector.className = 'version-selector';
-  selector.innerHTML = `
+    // Version selector
+    var versions = window.$docsify.versions;
+    var selector = document.createElement('div');
+    selector.className = 'version-selector';
+    selector.innerHTML = `
   <select>
       ${versions
       .map(
@@ -44,69 +44,69 @@ function initVersionSelector() {
       .join('')}
   </select>
   `;
-  
-  // Adding event listener
-  var versionPath = (window.location.hash && window.location.hash.split("?id=")[0].split('/')[1]) || defaultVersion;
-  selector.querySelector('select').value = versionPath;
-  selector.querySelector('select').addEventListener('change', function () {
-      updateVersion(this.value);
-  });
 
-  // Adding label
-  var labelText = 'Version:';
-  var label = document.createElement('span');
-  label.className = 'version-selector-label';
-  label.textContent = labelText;
-  selector.insertBefore(label, selector.querySelector('select'));
-  
-  var nameEl = document.querySelector('.app-name');
-  if (nameEl) {
-      var versionLabel = versions.find((v) => v.folder === versionPath).label;
-      nameEl.innerHTML += ` <small id="vlabel">${versionLabel}</small>`;
-      nameEl.parentNode.insertBefore(selector, nameEl.nextElementSibling);
-  }
-  return selector;
+    // Adding event listener
+    var versionPath = (window.location.hash && window.location.hash.split("?id=")[0].split('/')[1]) || defaultVersion;
+    selector.querySelector('select').value = versionPath;
+    selector.querySelector('select').addEventListener('change', function() {
+        updateVersion(this.value);
+    });
+
+    // Adding label
+    var labelText = 'Version:';
+    var label = document.createElement('span');
+    label.className = 'version-selector-label';
+    label.textContent = labelText;
+    selector.insertBefore(label, selector.querySelector('select'));
+
+    var nameEl = document.querySelector('.app-name');
+    if (nameEl) {
+        var versionLabel = versions.find((v) => v.folder === versionPath).label;
+        nameEl.innerHTML += ` <small id="vlabel">${versionLabel}</small>`;
+        nameEl.parentNode.insertBefore(selector, nameEl.nextElementSibling);
+    }
+    return selector;
 }
 
 function updateVersion(version) {
-  window.location.replace(window.$docsify.home + "#/" + version + "/");
+    window.location.replace(window.$docsify.home + "#/" + version + "/");
 
-if(document.getElementById("vlabel") != null)
-   document.getElementById("vlabel").innerText = window.$docsify.versions.find((v) => v.folder === version).label;
+    if (document.getElementById("vlabel") != null)
+        document.getElementById("vlabel").innerText = window.$docsify.versions.find((v) => v.folder === version).label;
 }
 
-(function () {
-  // Add plugin to docsify's plugin array
-  $docsify = $docsify || {};
-  $docsify.plugins = [].concat($docsify.plugins || [], DAGPlugin);
+(function() {
+    // Add plugin to docsify's plugin array
+    $docsify = $docsify || {};
+    $docsify.plugins = [].concat($docsify.plugins || [], DAGPlugin);
 })();
 
 (function() {
-  if (window.location.pathname === window.$docsify.home || window.location.pathname === window.$docsify.home + 'index.html' || window.location.hash === '/#' || window.location.hash === '/#/') {
-var existiert = false;
-for(var i = 0; window.$docsify.versions.length; i++) {
- if (window.location.hash == "/#/" + window.$docsify.versions[i].folder + "/"){
-return;
-}
+    if (window.location.pathname === window.$docsify.home || window.location.pathname === window.$docsify.home + 'index.html' || window.location.hash === '/#' || window.location.hash === '/#/') {
+        var existiert = false;
+        for (var i = 0; $docsify.versions.length; i++) {
+            if (window.location.hash == "/#/" + $docsify.versions[i].folder + "/") {
+                return;
+            }
 
 
-if (window.location.hash == "/#/" + window.$docsify.versions[i].folder){
-window.location.replace(window.$docsify.home + '#/' + window.$docsify.versions[i].folder + '/');
-break;
-}
-}
-    var defaultVersion = window.$docsify.versions.find((v) => v.default).folder;
-    window.location.replace(window.$docsify.home + '#/' + defaultVersion + '/');
-  }
+            if (window.location.hash == "/#/" + $docsify.versions[i].folder) {
+                window.location.replace(window.$docsify.home + '#/' + $docsify.versions[i].folder + '/');
+                break;
+            }
+        }
+        var defaultVersion = $docsify.versions.find((v) => v.default).folder;
+        window.location.replace(window.$docsify.home + '#/' + defaultVersion + '/');
+    }
 })();
 
 function getVersionName() {
-var loc = window.location.hash.replace("/#/", "").split("/")[0];
+    var loc = window.location.hash.replace("/#/", "").split("/")[0];
 
-var name = window.$docsify.versions.find((v)=> v.folder == loc).label;
+    var name = $docsify.versions.find((v) => v.folder == loc).label;
 
-if (name)
-  return name;
- 
-return;
+    if (name)
+        return name;
+
+    return;
 }
